ee08fbcd59a9d185e597623e04140f70831a8583
Diego Lis <diegolis@infoxel.com>
2019-11-06 10:43:36 -0300

    rename

--- {{cookiecutter.project_slug}}/api/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-
-class {{cookiecutter.model_name}}(models.Model):
-    pass
--- {{cookiecutter.project_slug}}/api/urls.py
+++ {{cookiecutter.project_slug}}/api/urls.py
@@ -1,9 +1,21 @@
-from rest_framework.routers import SimpleRouter
-from .views import {{cookiecutter.model_name}}ViewSet
-from django.conf.urls import url
+from django.conf import settings
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.contrib import admin
+import django_cas_ng.views as cas_views
 
-router = SimpleRouter()
+urlpatterns = [
+    path('api/', include("api.urls")),
+    path('accounts/login/', cas_views.LoginView.as_view(), name='cas_ng_login'),
+    path('accounts/logout/', cas_views.LogoutView.as_view(), name='cas_ng_logout'),
+    path(
+        'accounts/callback/',
+        cas_views.CallbackView.as_view(),
+        name='cas_ng_proxy_callback'
+    ),
+    path('admin/logout/', cas_views.LogoutView.as_view(), name='cas_ng_logout'),
+    path(settings.ADMIN_URL, admin.site.urls),
 
-router.register(r'{{cookiecutter.model_name.lower()}}', {{cookiecutter.model_name}}ViewSet)
+    # Your stuff: custom urls includes go here
 
-urlpatterns = router.urls
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
