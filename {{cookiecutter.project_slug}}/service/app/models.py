01e59e04fb70ada899fd82592e9981609f82d076
Diego Lis <diegolis@infoxel.com>
2019-11-08 03:17:34 -0300

    push

--- {{cookiecutter.project_slug}}/service/app/models.py
+++ {{cookiecutter.project_slug}}/service/app/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 class {{cookiecutter.model_name}}(models.Model):
     pass
-{% elif cookiecutter.db == 'elastic'}
+{% elif cookiecutter.db == 'elastic' %}
 class {{cookiecutter.model_name}}(Document):
 
     class Index:
         name = '{{cookiecutter.model_name.lower}}'
-{% elif cookiecutter.db == 'clickhouse'}
+{% elif cookiecutter.db == 'clickhouse' %}
 from infi.clickhouse_orm.database import Database
 from infi.clickhouse_orm.models import Model
 from infi.clickhouse_orm.fields import *
